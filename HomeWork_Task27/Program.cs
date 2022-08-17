b83762a4a91db44e2edd03d54bbc756ab0a2c70b
Vlad Muslimov <muslimov.vlad@mail.ru>
2022-08-17 21:24:22 +0500

    done task27

---
+++ HomeWork_Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+using System;
+
+class Pogram
+{
+    static int SumOfDigits(int numb)
+    {
+        int result = 0;
+        while (numb > 0)
+        {
+            result += numb % 10;
+            numb /= 10;
+        }
+        return result;
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Введите чило: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.Write($"Сумма цифер числа {number} = {SumOfDigits(number)}");
+    }
+}
