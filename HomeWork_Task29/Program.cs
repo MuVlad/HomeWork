5513c0007ee409db797110e922758aa0b3b2c4cd
Vlad Muslimov <muslimov.vlad@mail.ru>
2022-08-17 22:02:41 +0500

    done task29

---
+++ HomeWork_Task29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+using System;
+
+class Program
+{
+    static void ShowArray(int[] ar)
+    {
+        for (int i = 0; i < ar.Length; i++)
+        {
+            Console.WriteLine($"array[{i}] = {ar[i]}");
+        }
+    }
+    static int[] CreaterArray(){
+        Console.Write("Задайте размер массива: ");
+        int length = Convert.ToInt32(Console.ReadLine());
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write($"Введите элемент под индексом {i} : ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        return array;
+    }
+    static void Main(string[] args)
+    {
+        ShowArray(CreaterArray());
+    }
+}
