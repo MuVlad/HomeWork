557612c5cea2431e8d6489b574e080150bbe45e3
Vlad Muslimov <muslimov.vlad@mail.ru>
2022-09-01 16:20:59 +0500

    done task54

---
+++ HomeWork_Task54/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 54: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2      4 1 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+using System;
+class Program
+{
+    static int[,] sortArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                for (int k = 0; k < array.GetLength(1) - 1; k++)
+                {
+                    if (array[i, k] < array[i, k + 1])
+                    {
+                        int temp = array[i, k + 1];
+                        array[i, k + 1] = array[i, k];
+                        array[i, k] = temp;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+    static void printArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    static void Main(string[] args)
+    {
+        int[,] array = {{1,4,7,2},
+                        {5,9,2,3},
+                        {8,4,2,4}};
+
+        printArray(sortArray(array));
+    }
+}
