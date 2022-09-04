33a9157b773eafdac53c32a3d37407cf58b716be
Vlad Muslimov <muslimov.vlad@mail.ru>
2022-09-04 17:48:31 +0500

    updated task60

--- HomeWork_Task60/Program.cs
+++ HomeWork_Task60/Program.cs
@@ -5,12 +5,57 @@
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
+
 using System;
 class Program
 {
-    static void Main(string[] args)
+    static int[,,] createArray()
     {
         int[,,] array = new int[2, 2, 2];
 
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                for (int k = 0; k < array.GetLength(2); k++)
+                {
+                    array[i, j, k] = uniqueNumber(array);
+                }
+            }
+        }
+        return array;
+    }
+    static int uniqueNumber(int[,,] array)
+    {
+        int num = new Random().Next(10, 99);
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                for (int k = 0; k < array.GetLength(2); k++)
+                {
+                    if (array[i, j, k] == num) num = uniqueNumber(array);
+                }
+            }
+        }
+        return num;
+    }
+    static void printArray(int[,,] array)
+    {
+        for (int k = 0; k < array.GetLength(2); k++)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j, k]}({i}{j}{k})");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    static void Main(string[] args)
+    {
+        printArray(createArray());
     }
 }
