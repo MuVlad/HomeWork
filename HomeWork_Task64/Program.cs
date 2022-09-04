d46c1ba24707ff0628b24f1f843a5d321c33c773
Vlad Muslimov <muslimov.vlad@mail.ru>
2022-09-04 18:18:48 +0500

    done task64

---
+++ HomeWork_Task64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8"
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число M: ");
+        int m = int.Parse(Console.ReadLine());
+        Console.Write("Введите число N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = m; i <= n; i++)
+        {
+            Console.Write(i + " ");
+        }
+    }
+}
